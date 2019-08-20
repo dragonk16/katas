8f7c9f215a8ea1c22c8fc08bba2fda61124c9596
davidbr141 <davidbr141@gmail.com>
2019-08-21 01:22:11 +0300

    solved

--- CPP/DirectionsReduction.cpp
+++ CPP/DirectionsReduction.cpp
@@ -7,23 +7,27 @@
 
 
 
-enum directions { North = -1, South, East, West};
+
 
 int DirReduction::test() {
-    std::vector<std::string> d1 = {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"};
+    std::vector<std::string> d1 = {"EAST", "EAST", "WEST","EAST", "WEST", "WEST", "NORTH"};
     std::vector<std::string> ans1 = DirReduction::dirReduc(d1);
     std::vector<std::string> sol1 = {"WEST"};
-    for (int i = 0; ans1.size() > i ; ++i) {
-        cout<<ans1[i]<<" ,";
+    cout << "sol - ans1" << endl;
+    cout << sol1.size() << "   " << ans1.size() << endl;
+    for (int i = 0; ans1.size() > i; ++i) {
+        cout << (sol1[i] == ans1[i]) << endl;
     }
-    cout<<endl;
-    std::vector<std::string> d2 = {"NORTH","SOUTH","SOUTH","EAST","WEST","NORTH", "NORTH"};
-    std::vector<std::string> ans2 = DirReduction::dirReduc(d2);
-    std::vector<std::string> sol2 = {"NORTH"};
-    for (int i = 0; ans2.size() > i ; ++i) {
-        cout<<ans2[i]<<" ,";
-    }
-    cout<<endl;
+    cout << endl;
+//    std::vector<std::string> d2 = {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "NORTH"};
+//    std::vector<std::string> ans2 = DirReduction::dirReduc(d2);
+//    std::vector<std::string> sol2 = {"NORTH"};
+//    cout << "sol - ans2" << endl;
+//    cout << sol2.size() << "   " << ans2.size() << endl;
+//    for (int i = 0; ans2.size() > i; ++i) {
+//        cout << (sol2[i] == ans2[i]) << endl;
+//    }
+//    cout << endl;
 }
 
 vector<char>* DirReduction::StringToChar(vector<string> &str) {
@@ -63,11 +67,81 @@ vector<string>* DirReduction::CharToString(vector<char> &str) {
     }
     return Vec;
 }
-vector<string> DirReduction::dirReduc(vector<string> &arr){
-    vector<char> *current = StringToChar(arr);
-    vector<char> *next = new vector<char>;
-    
+
+int nextValid(vector<bool> valid,int index) {
+    index++;
+    while (index < valid.size() && valid[index] != 1) {
+        index++;
+    }
+    if (index >= valid.size()) {
+        return -1;
+    }
+    return index;
 }
+
+int previousValid(vector<bool> valid,int index) {
+    index--;
+    while (index >= 0 && valid[index] != 1) {
+        index--;
+    }
+    if (index < 0) {
+        return -1;
+    }
+    return index;
+}
+
+vector<string> DirReduction::dirReduc(vector<string> &arr) {
+    vector<char> *current = StringToChar(arr);//TODO delete this
+    vector<bool> valid;
+    valid.assign(arr.size(), 1);
+    int right = 1, left = 0;
+    while (right != -1) {
+        if (left == -1) {
+            left = right;
+            right = nextValid(valid, right);
+            if (right == -1) {
+                break;
+            }
+        }
+        if ((*current)[left] == 'N' && (*current)[right] == 'S') {
+            valid[right] = valid[left] = 0;
+            left = previousValid(valid, left);
+            right = nextValid(valid, right);
+            continue;
+        }
+        if ((*current)[left] == 'S' && (*current)[right] == 'N') {
+            valid[right] = valid[left] = 0;
+            left = previousValid(valid, left);
+            right = nextValid(valid, right);
+            continue;
+        }
+        if ((*current)[left] == 'E' && (*current)[right] == 'W') {
+            valid[right] = valid[left] = 0;
+            left = previousValid(valid, left);
+            right = nextValid(valid, right);
+            continue;
+        }
+        if ((*current)[left] == 'W' && (*current)[right] == 'E') {
+            valid[right] = valid[left] = 0;
+            left = previousValid(valid, left);
+            right = nextValid(valid, right);
+            continue;
+        }
+        left=right;
+        right=nextValid(valid, right);
+    }
+    vector<string> *output = new vector<string>;
+    for (int i = 0; i < arr.size(); ++i) {
+        if (valid[i] == 1) {
+            output->push_back(arr[i]);
+        }
+    }
+    return *output;
+}
+
+
+//    vector<char> *next = new vector<char>;
+
 //vector<string> DirReduction::dirReduc(vector<string> &arr) {
 //
 //    vector<char> *current = StringToChar(arr);
